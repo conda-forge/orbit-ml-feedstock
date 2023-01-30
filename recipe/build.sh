e6df306caa1db2455463cf462bdbbf2d2ca891e8
edwinnglabs <edwinnglabs@gmail.com>
2023-01-30 12:37:40 -0800

    try for CXX setting

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,9 @@
 echo "Set CXX TYPE"
+STAN_MPI=true
+CXX=g++
 TBB_CXX_TYPE=gcc
 echo "Print python version"
 python -c "import sys; print(sys.version)"
 echo "Install pacakge."
 $PYTHON -m pip install . -vv
-echo "Finished installation."
+echo "Finished installation. Exit from build.sh."
