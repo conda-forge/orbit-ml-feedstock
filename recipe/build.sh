467d7b231980565fb8aa4728d8267bb58a0f4178
edwinnglabs <edwinnglabs@gmail.com>
2023-01-30 18:17:54 -0800

    print platform to double check

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,8 @@ CXX=mpicxx
 TBB_CXX_TYPE=gcc
 echo "Print python version"
 python -c "import sys; print(sys.version)"
+echo "Print platform"
+python -c "import platform; print(platform.platform())"
 echo "Install pacakge."
 $PYTHON -m pip install . -vv
 echo "Finished installation. Exit from build.sh."
