da6eab966a91b9b69eb47ccde29573bbc7badbae
edwinnglabs <edwinnglabs@gmail.com>
2024-03-22 01:47:45 -0700

    recover pip install with pruning

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,8 @@ echo "Print python version"
 python -c "import sys; print(sys.version)"
 echo "Print platform"
 python -c "import platform; print(platform.platform())"
+echo "Install pacakge."
+$PYTHON -m pip install . -vv
+echo "Prune compiled files."
+find ./orbit/stan/ -type f ! -name "*.stan" -delete
 echo "Finished installation. Exit from build.sh."
